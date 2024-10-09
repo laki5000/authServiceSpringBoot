787edd2e020253ecf5e837a21bbb84d0cf9ecdc3
Lakatos Bence <lakatos816@gmail.com>
2024-10-09 17:50:07 +0200

    init :)

--- src/main/java/com/example/domain/user/controller/IUserController.java
+++ src/main/java/com/example/domain/user/controller/IUserController.java
@@ -1,9 +1,4 @@
 package com.example.domain.user.controller;
 
-import com.example.base.controller.IBaseController;
-import com.example.domain.user.dto.request.UserCreateRequestDTO;
-import com.example.domain.user.dto.request.UserUpdateRequestDTO;
-
 /** Controller interface for user controller. */
-public interface IUserController
-        extends IBaseController<UserCreateRequestDTO, UserUpdateRequestDTO> {}
+public interface IUserController {}
